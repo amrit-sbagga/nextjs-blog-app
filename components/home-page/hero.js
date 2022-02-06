ab63772c80058e0e551591f284992d69ec793077
Amrit Pal Singh <amrit.s@proximity.tech>
2022-02-06 19:47:31 +0530

    Added Hero component

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
+import classes from "./hero.module.css";
 
 export default function Hero() {
-  return <div></div>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/amrit.jpg"
+          alt="image showing Amrit"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Amrit</h1>
+      <p>I blog about web and cloud application development.</p>
+    </section>
+  );
 }
