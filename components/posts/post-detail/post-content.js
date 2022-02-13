2a818fff10708e2c19c870d65af763fe1b8553a7
Amrit Pal Singh <amrit.s@proximity.tech>
2022-02-13 17:49:02 +0530

    modified post content to markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import classes from './post-content.module.css';
 import PostHeader from './post-header';
+import ReactMarkdown from 'react-markdown';
 
-const DUMMY_POST = [
+const DUMMY_POST = 
   {
     slug : 'getting-started-with-nextjs',
     title : 'Getting Started with NextJS',
@@ -10,14 +11,13 @@ const DUMMY_POST = [
     date : '2022-02-13',
     content : '# This is a first post'
   }
-]
 
 function PostContent() {
   const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath}/>
-        {DUMMY_POST.slug}
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   )
 }
